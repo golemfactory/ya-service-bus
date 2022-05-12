c421bc326ebcef0e6bbc7c4d7fc152c0f639d46b
scx1332 <sieciech.czajka@golem.network>
2022-05-12 11:47:27 +0200

    Stub tokio project

--- crates/data-limiter/src/main.rs
+++ crates/data-limiter/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use tokio;
+use structopt::StructOpt;
+use dotenv;
+use env_logger;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "options", about = "Options for data limiter relay")]
+struct Opt {
+    // we don't want to name it "speed", need to look smart
+    #[structopt(default_value = "127.0.0.1:111")]
+    target_addr: String,
+}
+
+
+async fn run() -> anyhow::Result<()> {
+    let opt = Opt::from_args();
+    dotenv::dotenv().ok();
+
+    log::debug!("Starting relay. Target addr: {}", opt.target_addr);
+
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    env_logger::init();
+
+    match run().await {
+        Ok(()) => {
+
+        }
+        Err(err) => {
+
+        }
+    }
+
 }
