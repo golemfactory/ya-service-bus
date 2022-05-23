0fc0506093326f2ce4abbf11573ffd182be10ff9
scx1332 <sieciech.czajka@golem.network>
2022-05-23 16:53:32 +0200

    working on data limiter

--- crates/data-limiter/src/main.rs
+++ crates/data-limiter/src/main.rs
@@ -1,39 +1,98 @@
+use anyhow::anyhow;
 use dotenv;
 use env_logger;
 use futures::future::{AbortHandle, Abortable};
+use std::sync::{Arc, Mutex};
 use structopt::StructOpt;
 use tokio;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf};
 use tokio::net::{TcpListener, TcpStream};
+
+use figment::{
+    providers::{Env, Format, Serialized, Toml},
+    Figment, Profile,
+};
+use rocket::data::{ByteUnit, Limits};
+use rocket::fairing::AdHoc;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::{get, routes};
+struct TransportLoopData {
+    _synched: i32,
+    transferred: u64,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Config {
+    app_value: usize,
+    /* and so on.. */
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config { app_value: 3 }
+    }
+}
+
+async fn transport_loop(
+    mut rd: ReadHalf<TcpStream>,
+    mut wd: WriteHalf<TcpStream>,
+    shared_data: Arc<Mutex<TransportLoopData>>,
+) -> anyhow::Result<()> {
+    let mut buf = vec![0; 16];
+
+    loop {
+        let n = match rd.read(&mut buf).await {
+            Ok(n) => n,
+            Err(err) => {
+                log::error!("Error when reading {:?}", err);
+                break;
+            }
+        };
+
+        {
+            let mut shared_data = shared_data.lock().unwrap();
+            shared_data.transferred += n as u64;
+        }
+
+        if n == 0 {
+            log::error!("Read 0 bytes");
+            break;
+        }
+        println!("GOT {:?}", &buf[..n]);
+        wd.write_all(&buf[..n]).await.unwrap();
+    }
+    Ok(())
+}
 
 async fn process_socket(socket: TcpStream, target: TcpStream) -> anyhow::Result<()> {
     // do work with socket here
-    let (mut rd, mut wr) = tokio::io::split(socket);
-    let (mut rdt, mut wrt) = tokio::io::split(target);
+    let (rd, wr) = tokio::io::split(socket);
+    let (rdt, wrt) = tokio::io::split(target);
     let (abort_handle, abort_registration) = AbortHandle::new_pair();
     let (abort_handle2, abort_registration2) = AbortHandle::new_pair();
+
+    let sent_data = Arc::new(Mutex::new(TransportLoopData {
+        _synched: 1,
+        transferred: 1,
+    }));
+    let receive_data = Arc::new(Mutex::new(TransportLoopData {
+        _synched: 1,
+        transferred: 1,
+    }));
 
     tokio::spawn(async move {
         let reader_future = Abortable::new(
             async move {
-                let mut buf = vec![0; 16];
-
-                loop {
-                    let n = match rd.read(&mut buf).await {
-                        Ok(n) => n,
-                        Err(err) => {
-                            log::error!("Error when reading {:?}", err);
-                            break;
-                        }
-                    };
-
-                    if n == 0 {
-                        log::error!("Read 0 bytes");
-                        break;
+                match transport_loop(rd, wrt, sent_data).await {
+                    Ok(()) => {
+                        log::info!("Transport loop finished");
                     }
-                    println!("GOT {:?}", &buf[..n]);
-                    wrt.write_all(&buf[..n]).await.unwrap();
+                    Err(err) => {
+                        log::error!("Transport loop finished with error: {:?}", err);
+                    }
                 }
+
                 abort_handle2.abort();
             },
             abort_registration,
@@ -49,7 +108,18 @@ async fn process_socket(socket: TcpStream, target: TcpStream) -> anyhow::Result<
     });
     let writer_future = Abortable::new(
         async move {
+            match transport_loop(rdt, wr, receive_data).await {
+                Ok(()) => {
+                    log::info!("Transport loop finished");
+                }
+                Err(err) => {
+                    log::error!("Transport loop finished with error: {:?}", err);
+                }
+            }
+
+            /*
             let mut buf = vec![0; 16];
+
 
             loop {
                 let n = match rdt.read(&mut buf).await {
@@ -67,7 +137,7 @@ async fn process_socket(socket: TcpStream, target: TcpStream) -> anyhow::Result<
                 let s = String::from_utf8_lossy(&buf[..n]);
                 println!("GOT from server {:?}", s);
                 wr.write_all(&buf[..n]).await.unwrap();
-            }
+            }*/
             abort_handle.abort();
         },
         abort_registration2,
@@ -94,11 +164,65 @@ struct Opt {
     // we don't want to name it "speed", need to look smart
     #[structopt(long = "--target-addr", default_value = "127.0.0.1:17777")]
     target_addr: String,
+    // we don't want to name it "speed", need to look smart
+    #[structopt(long = "--rest-api-addr", default_value = "127.0.0.1:22000")]
+    rest_api_addr: String,
+}
+
+#[get("/world")] // <- route attribute
+fn world() -> &'static str {
+    // <- request handler
+    "hello, world!"
 }
 
 async fn run() -> anyhow::Result<()> {
     let opt = Opt::from_args();
     dotenv::dotenv().ok();
+
+    log::debug!("Run ...");
+
+    let str: String = opt.rest_api_addr;
+    let split: Vec<&str> = str.split(":").collect();
+
+    let address = split.get(0).ok_or(anyhow!("failed to split address"))?;
+    let port_str = split.get(1).ok_or(anyhow!("failed to split address"))?;
+    let port = port_str.parse::<u16>()?;
+    /*
+        log::debug!("Applying configuration to rocket rest api address: {} port: {}", address, port);
+        let figment = Figment::from(rocket::Config::default())
+            .merge(("port", port))
+            .merge(("address", address))
+            .merge(("limits", Limits::new().limit("json", ByteUnit::from(2000000))));
+
+        log::debug!("Starting rocket at api address: {} port: {}", address, port);
+        let rock = rocket::custom(figment).mount("/api", routes![world]).attach(AdHoc::config::<Config>()).launch().await?;
+    */
+
+    let cfg = Figment::from(rocket::config::Config::default()).merge(("port", port));
+    rocket::custom(cfg)
+        .mount("/api", routes![world])
+        .launch()
+        .await?;
+
+    let rocket = rocket::build();
+    let figment = rocket.figment();
+
+    #[derive(Deserialize)]
+    #[serde(crate = "rocket::serde")]
+    struct Config {
+        port: u16,
+    }
+
+    // extract the entire config any `Deserialize` value
+    let config: Config = figment.extract().expect("config");
+
+    // or a piece of it into any `Deserialize` value
+    //let custom: Vec<String> = figment.extract_inner("custom").expect("custom");
+    /*
+    let _rocket = rocket::build()
+        .mount("/api", routes![world])
+        .launch()
+        .await?;*/
 
     log::debug!(
         "Starting relay. Listen addr: {}, target addr: {}",
